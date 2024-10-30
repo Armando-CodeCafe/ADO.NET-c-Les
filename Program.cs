d15ea5562b39dd426f5dbccf194ce836f07e22cd
armandocodecafe <armando.roerdinkveldboom@code-cafe.nl>
2024-10-30 15:47:17 +0100

    added comments

--- Program.cs
+++ Program.cs
@@ -5,11 +5,12 @@ class Program
     static void Main(string[] args)
     {
 
-
+        //login menu
         Console.WriteLine("Do you want to login or register?");
         string choice = Console.ReadLine();
         if (choice == "register")
         {
+            //maak een user object aan voor de nieuwe gebruiker
             User u = new User();
             Console.WriteLine("Please fill in your username");
             u.username = Console.ReadLine();
@@ -17,20 +18,23 @@ class Program
             u.email = Console.ReadLine();
             Console.WriteLine("Please fill in your password");
             u.password = Console.ReadLine();
+            //sla de gebruiker op in de database met een (C)rud functie op de User
             u.Create();
         }
         else if (choice == "login")
         {
+            //maak een user object aan die de ingelogde user gaat opslaan
             User loggedInUser = new User();
             Console.WriteLine("Please fill in your email");
             loggedInUser.email = Console.ReadLine();
             Console.WriteLine("Please fill in your password");
             loggedInUser.password = Console.ReadLine();
+            //gebasseerd op of de user correct of incorrect heeft ingevuld, maak een boolean met de waarde true of false
             bool loggedIn = loggedInUser.Login();
+            //zolang de gebruiker ingelogd is, blijf het update menu laten zien
             while (loggedIn)
             {
 
-                //gebruiker is succesvol ingelogd 
                 Console.WriteLine($"Logged in as user {loggedInUser.username} with email {loggedInUser.email} and id {loggedInUser.id}\n");
 
                 Console.WriteLine("Choose what you want to do:\n1. Change username\n2. Change email\n3. Change password");
@@ -38,10 +42,13 @@ class Program
                 string option = Console.ReadLine();
                 if (option == "1")
                 {
+                    //edit de gebruikers naam met een nieuwe input
                     Console.WriteLine("Please fill in your new username");
                     loggedInUser.username = Console.ReadLine();
+                    //update dit zodat het ook in de database gebeurt
                     loggedInUser.Update();
                 }
+                //alle code hieronder is hetzelfde als optie 1 maar dan met een andere waarde
                 else if (option == "2")
                 {
                     Console.WriteLine("Please fill in your new email");
--- User.cs
+++ User.cs
@@ -7,22 +7,27 @@ class User
     public string email;
     public string password;
     public bool isAdmin;
+    //(C)rud functie, pakt de gegevens van de user waar de functie op wordt aangeroepen en voert een insert query ermee uit
     public void Create()
     {
+        //database connect
         MySqlConnection connection = new MySqlConnection(
             "Server=localhost;Database=NativeDb;User=root;"
             );
         connection.Open();
-
+        //query initializatie
         MySqlCommand command = connection.CreateCommand();
         command.CommandText = "INSERT INTO Users(username,email,password) VALUES(@username,@email,@password)";
+        //variabelen en parameters invullen voor de query
         command.Parameters.AddWithValue("@username", username);
         command.Parameters.AddWithValue("@email", email);
         command.Parameters.AddWithValue("@password", password);
+        //voer de query uit, maar verwacht geen rijen of resultaten
         command.ExecuteNonQuery();
+        //sluit de database weer af
         connection.Close();
     }
-
+    //C(R)UD kijk of de huigide email en password in de database zitten bij een gebruiker, en return dan true of false als het gelukt is
     public bool Login()
     {
         MySqlConnection connection = new MySqlConnection(
@@ -34,16 +39,19 @@ class User
         command.CommandText = "SELECT * FROM Users WHERE email = @email AND password = @password";
         command.Parameters.AddWithValue("@email", email);
         command.Parameters.AddWithValue("@password", password);
+        //dit keer moeten we wel uitlezen dus gebruiken we reader
         MySqlDataReader reader = command.ExecuteReader();
-
+        //als onze query een database resultaat had zal deze functie true terug geven
         if (reader.Read())
         {
+            //update de id en username van de user instantie in ons script met de id en username uit de database
             id = (int)reader["id"];
             username = (string)reader["username"];
             connection.Close();
 
             return true;
         }
+        //er was geen gebruiker met die gegevens
         else
         {
             Console.WriteLine("login info incorrect");
@@ -53,6 +61,7 @@ class User
         }
 
     }
+    //CR(U)D bijna hetzelfde als Create, maar update de gebruiker inplaats van een nieuwe te inserten. Hiervoor is het wel belangrijk dat de user.id variable geset is
     public void Update()
     {
         MySqlConnection connection = new MySqlConnection(
